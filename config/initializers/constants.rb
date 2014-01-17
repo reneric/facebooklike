035bb5e34c820cf326a9ceed2ceb5614882658b8
Ren Simmons <rsimmo4@gmail.com>
2014-01-16 23:14:03 -0600

    quick heroku push

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,4 @@
 APP_ID= '125644104300196' # please change!
 APP_SECRET= 'e7e20d81b70d12c80aad94a85853f0db' # please change!
-SITE_URL = 'http://localhost:3000'
+SITE_URL = 'http://facebooklike.herokuapp.com'
+# SITE_URL = 'http://localhost:3000'
